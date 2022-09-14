0ecf8f6fc0b1aedf4fc28dbc4f444cbba277be83
James <5000win@naver.com>
2022-09-14 23:01:32 +0900

    최종

--- assets/js/common.js
+++ assets/js/common.js
@@ -154,11 +154,11 @@ $(window).resize(function () {
       $(this)
         .attr("href", "javascript:void(0)")
         .next()
-        .slideToggle("slow")
+        .slideDown()
         .parent()
         .siblings()
         .find(".depth2")
-        .slideUp("slow");
+        .slideUp();
       // $(this)
       //   .parent()
       //   .addClass("is-active")
